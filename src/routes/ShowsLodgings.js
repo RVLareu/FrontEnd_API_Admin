69a5f02bed6448d620aec0455dd0de9624d8a2c2
RVLareu <rlareu@fi.uba.ar>
2022-10-16 14:44:58 -0300

    hago mas compacto showLodgings

--- src/routes/ShowsLodgings.js
+++ src/routes/ShowsLodgings.js
@@ -2,30 +2,21 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import data from '../data/dataProperties';
 import Card from "../components/Card"
-import Tarjetas from "../components/Tarjetas"
-
-
+import { Box } from '@mui/material';
 
 
 
 const ShowsLodgings = () => {
-    const cards = data.map(item => {
-
-        return (
-          <Card
-            key={item.id}
-            {...item}
-          />
-        )
-      })
-    
       return (
-        <div>
-         
-            {cards}
-    
-          
-        </div>
+        <>
+          <Box sx={{display:'flex',flexWrap: 'wrap' }}>
+              {data.map(item => {
+                return (
+                    <Card key={item.id} {...item}/>
+                )}
+              )}   
+          </Box>
+        </>
       )
 }
 
