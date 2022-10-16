feeb279ac4cb420679a26b348e992b4eee3b8fce
RVLareu <rlareu@fi.uba.ar>
2022-10-16 14:44:37 -0300

    creo componente navBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Home from '@mui/icons-material/Home';
+import { IconButton } from '@mui/material';
+import { Link } from "react-router-dom";
+
+export default function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{background:'#dc9c14'}}>
+        <Toolbar>
+
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            HospedateAhora
+          </Typography>
+
+        <Button>
+            <Link style={{ textDecoration: 'none' }} to="/">
+                    Publicaciones
+            </Link>
+        </Button>
+        <Button>
+            <Link style={{ textDecoration: 'none' }} to="/showLodgings">
+                    Propiedades
+            </Link>
+        </Button>
+            <IconButton>
+                    <Home/>
+            </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
