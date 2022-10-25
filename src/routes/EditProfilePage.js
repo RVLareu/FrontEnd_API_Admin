e154cb473dd3fa77789f4ec4678da611e96b8792
Luciano <lucianortiz97@gmail.com>
2022-10-25 19:39:04 -0300

    Actualizacion de perfil

--- src/routes/EditProfilePage.js
+++ src/routes/EditProfilePage.js
@@ -2,17 +2,20 @@ import React, {useEffect, useState} from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import axios from '../api/axios';
 import Card from "../components/Card";
-import { Avatar, Box, Button, Divider, Paper, Rating, TextField, Typography } from '@mui/material';
+import { Avatar, Box, Button, CircularProgress, Divider, Paper, Rating, TextField, Typography } from '@mui/material';
 import { Add, ContactMail, ContactMailOutlined, Description, DescriptionOutlined, Edit, LocationCity, LocationOn, LocationOnOutlined, PersonOutline, VerifiedOutlined, VerifiedUserOutlined, Work, WorkOutline, WorkOutlineOutlined } from '@mui/icons-material';
 
 
-
+import swal from 'sweetalert2';
 
 const EditProfile = () => {
+
 
   const navigate = useNavigate();
 
   let username = window.localStorage.getItem("username")
+
+  const [profileData, setProfileData] = useState();
     
   if (!username){
     console.log("no autorizado")
@@ -21,63 +24,105 @@ const EditProfile = () => {
     return;
   } 
 
-  // useEffect(() => {
-  //   //loadLodgings();
-  //   }, []);
+  if(!profileData){
+    const params = new URLSearchParams([['user_email', username]]);
+
+    axios.get('/getProfile/',{ params: params })
+    .then((response) => {
+      setProfileData(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
+
+  const updateProfile = async(e) => {
+    e.preventDefault()
+    const form = new FormData(e.currentTarget);
+
+    const body = {
+        'email': username, 'name': form.get("name"), 'bio': form.get("bio"), 'location': form.get("location"), 'ocupation': form.get("ocupation")
+    }
+
+    const headers = { 'Content-Type': 'application/json' }
+
+
+    axios.put('/updateProfile/', JSON.stringify(body),{headers: headers})
+    .then((response) => {
+      swal.fire({title:"Exito", text: "Perfil actualizado exitosamente", icon:"success"}).then(() => {navigate("/profile")})
+    })
+    .catch((error) => {
+      console.log(error);
+      swal.fire({title:"Error", text: error, icon:"error"})
+    });
+  }
 
 
   return (
     <Paper sx={{width: 700, paddingBottom: "30px", display: 'flex', flexDirection:'column', alignItems:'center', textAlign: 'left'}}
-      component="form"
       noValidate
+      component="form"
+      onSubmit={(e) => updateProfile(e)}
       autoComplete="off">
-      <div style={{width: "60%",
-    display: "flex",
-    justifyContent: "space-around",
-    alignItems: "center"}}>
-      <Avatar
-        alt={username.toLocaleUpperCase()}
-        src="broken-image.jpg"
-        sx={{ width: 100, height: 100 , marginTop: "10px", marginBottom: "20px"}}
-      />
-      <Button sx={{height: "50px"}}> Cambiar imagen </Button>
-      </div>
+      {profileData ? <>
 
-      <TextField
-          id="outlined-read-only-input"
-          label="Nombre"
-          defaultValue="Nombre"
-          sx={{margin: "10px 0 10px 0", width: "450px"}}
-        />
+          <div style={{width: "60%",
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center"}}>
+          <Avatar
+            alt={username.toLocaleUpperCase()}
+            src="broken-image.jpg"
+            sx={{ width: 100, height: 100 , marginTop: "10px", marginBottom: "20px"}}
+          />
+          <Button sx={{height: "50px"}} onClick={(e) => {e.preventDefault();}}> Cambiar imagen </Button>
+          </div>
 
-      <TextField
-          id="outlined-read-only-input"
-          label="Biografia"
-          defaultValue="Mi biografia"
-          sx={{margin: "10px 0 10px 0", width: "450px"}}
-          multiline
-        />
+          {/* <TextField
+              name="email"
+              label="E-mail"
+              defaultValue={profileData.email}
+              sx={{margin: "10px 0 10px 0", width: "450px"}}
+            /> */}
 
-      <TextField
-          id="outlined-read-only-input"
-          label="Ocupación"
-          defaultValue="Mi ocupacion"
+          <TextField
+              name="name"
+              label="Nombre"
+              defaultValue={profileData.name}
+              sx={{margin: "10px 0 10px 0", width: "450px"}}
+            />
 
-          sx={{margin: "10px 0 10px 0", width: "450px"}}
+          <TextField
+              name="bio"
+              label="Biografia"
+              defaultValue={profileData.bio}
+              sx={{margin: "10px 0 10px 0", width: "450px"}}
+              multiline
+            />
 
-        />
+          <TextField
+              name="ocupation"
+              label="Ocupación"
+              defaultValue={profileData.ocupation}
 
-      <TextField
-          id="outlined-read-only-input"
-          label="Ubicación"
-          defaultValue=""
-          sx={{margin: "10px 0 10px 0", width: "450px"}}
+              sx={{margin: "10px 0 10px 0", width: "450px"}}
 
-        />
+            />
 
-      <Button sx={{marginTop: "10px"}} variant="contained" onClick={() => {alert("TO DO")}} >
-        Aceptar
-      </Button>
+          <TextField
+              name="location"
+              label="Ubicación"
+              defaultValue={profileData.location}
+              sx={{margin: "10px 0 10px 0", width: "450px"}}
+
+            />
+
+          <Button sx={{marginTop: "10px"}} variant="contained" type="submit">
+            Aceptar
+          </Button>
+        </>
+     :<CircularProgress></CircularProgress>}
   </Paper>
   
   )
--- src/routes/ProfilePage.js
+++ src/routes/ProfilePage.js
@@ -2,10 +2,8 @@ import React, {useEffect, useState} from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import axios from '../api/axios';
 import Card from "../components/Card";
-import { Avatar, Box, Button, Divider, Paper, Rating, Typography } from '@mui/material';
+import { Avatar, Box, Button, CircularProgress, Divider, Paper, Rating, Typography } from '@mui/material';
 import { Add, ContactMail, ContactMailOutlined, Description, DescriptionOutlined, Edit, LocationCity, LocationOn, LocationOnOutlined, PersonOutline, VerifiedOutlined, VerifiedUserOutlined, Work, WorkOutline, WorkOutlineOutlined } from '@mui/icons-material';
-
-
 
 
 const Profile = () => {
@@ -13,6 +11,8 @@ const Profile = () => {
   const navigate = useNavigate();
 
   let username = window.localStorage.getItem("username")
+
+  const [profileData, setProfileData] = useState();
     
   if (!username){
     console.log("no autorizado")
@@ -21,6 +21,18 @@ const Profile = () => {
     return;
   } 
 
+  if(!profileData){
+    const params = new URLSearchParams([['user_email', username]]);
+
+    axios.get('/getProfile/',{ params: params })
+    .then((response) => {
+      setProfileData(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
   // useEffect(() => {
   //   //loadLodgings();
   //   }, []);
@@ -28,7 +40,7 @@ const Profile = () => {
 
   return (
     <Paper sx={{width: 700, height: 600, display: 'flex', flexDirection:'column', alignItems:'center', textAlign: 'left'}}>
-
+      {profileData ? <>
       <Avatar
         alt={username.toLocaleUpperCase()}
         src="broken-image.jpg"
@@ -36,34 +48,36 @@ const Profile = () => {
       />
 
       <Typography sx={{width: '100%', padding: '0 20px 0 20px'}} variant="body2" gutterBottom>
-        <b ><VerifiedUserOutlined style={{transform: "translate(0px, 7px)"}}/> Mail: </b> {username}
+        <b ><VerifiedUserOutlined style={{transform: "translate(0px, 7px)"}}/> Mail: </b> {profileData.email}
       </Typography>
 
       <Divider component="div" sx={{width: "100%"}}/>
 
     <Typography sx={{width: '100%', padding: '0 20px 0 20px'}} variant="body2" gutterBottom>
-        <b ><PersonOutline style={{transform: "translate(0px, 7px)"}}/> Nombre: </b> sin completar 
+        <b ><PersonOutline style={{transform: "translate(0px, 7px)"}}/> Nombre: </b> {profileData.name || "sin completar"} 
       </Typography>
 
       <Divider component="div" sx={{width: "100%"}}/>
 
       <Typography sx={{width: '100%', padding: '0 20px 0 20px'}} variant="body2" gutterBottom>
-        <b ><DescriptionOutlined style={{transform: "translate(0px, 7px)"}}/> Biografía: </b> <span>sin completar</span> 
+        <b ><DescriptionOutlined style={{transform: "translate(0px, 7px)"}}/> Biografía: </b> <span> {profileData.bio || "sin completar"}</span> 
       </Typography>
 
       <Divider component="body1" sx={{width: "100%"}}/>
 
       <Typography sx={{width: '100%', padding: '0 20px 0 20px'}} variant="body2" gutterBottom>
-        <b><WorkOutline style={{transform: "translate(0px, 7px)"}}/> Ocupación: </b> <span>sin completar</span> 
+        <b><WorkOutline style={{transform: "translate(0px, 7px)"}}/> Ocupación: </b> <span>{profileData.ocupation || "sin completar"}</span> 
       </Typography>
 
       <Divider component="body1" sx={{width: "100%"}}/>
 
       <Typography sx={{width: '100%', padding: '0 20px 0 20px'}} variant="body2" gutterBottom>
-        <b ><LocationOnOutlined style={{transform: "translate(0px, 7px)"}}/> Ubicación: </b> <span>sin completar</span> 
+        <b ><LocationOnOutlined style={{transform: "translate(0px, 7px)"}}/> Ubicación: </b> <span>{profileData.location || "sin completar"}</span> 
       </Typography>
 
       <Divider component="body1" sx={{width: "100%"}}/>
+     
+      
 
       {/* <Typography sx={{width: '100%', padding: '0 20px 0 20px'}} variant="body2" gutterBottom>
 
@@ -74,7 +88,8 @@ const Profile = () => {
       <Button sx={{marginTop: "40px"}} variant="contained" onClick={() => navigate("/profile/edit")} startIcon={<Edit />}>
         Editar
       </Button>
-
+      </>
+     :<CircularProgress></CircularProgress>}
     </Paper>
   
   )
